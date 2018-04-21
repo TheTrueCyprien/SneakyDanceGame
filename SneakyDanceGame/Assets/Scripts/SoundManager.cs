7ebd7fd2fdc8836df444597e0f8a9aedadd16f0d
TheTrueCyprien <connor95@hotmail.de>
2018-04-21 20:32:27 +0200

    Added script for beat detection

---
+++ SneakyDanceGame/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    //the current position of the song (in seconds)
+    float songPosition;
+
+    //the current position of the song (in beats)
+    float songPosInBeats;
+
+    //the duration of a beat
+    float secPerBeat;
+
+    //how much time (in seconds) has passed since the song started
+    float dsptimesong;
+
+    //beats per minute of a song
+    float bpm = 100;
+
+    //keep all the position-in-beats of notes in the song
+    float[] notes;
+
+    //the index of the next note to be spawned
+    int nextIndex = 0;
+
+    // Use this for initialization
+    void Start () {
+        //calculate how many seconds is one beat
+        secPerBeat = 60f / bpm;
+
+        //record the time when the song starts
+        dsptimesong = (float)AudioSettings.dspTime;
+
+        //start the song
+        GetComponent<AudioSource>().Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //calculate the position in seconds
+        songPosition = (float)(AudioSettings.dspTime - dsptimesong);
+
+        //calculate the position in beats
+        songPosInBeats = songPosition / secPerBeat;
+
+        if (songPosInBeats > nextIndex)
+        {
+            GetComponentInChildren<Renderer>().material.color = new Color(255*(1-nextIndex%2), 0, 255*(nextIndex%2));
+
+            nextIndex++;
+        }
+    }
+}
