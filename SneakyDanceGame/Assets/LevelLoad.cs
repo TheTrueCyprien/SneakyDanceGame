c64e00055d815047ac0acf63272d01cdeb6659e9
jazgardev <jazgar.dev@gmail.com>
2018-04-22 13:27:27 +0200

    added smart collision for tilemap

---
+++ SneakyDanceGame/Assets/LevelLoad.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
+
+public class LevelLoad : MonoBehaviour {
+
+    public bool[,] tileMap;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += gatherTiles;
+    }
+
+
+    private void gatherTiles(Scene scene, LoadSceneMode mode)
+    {
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+}
---
+++ SneakyDanceGame/Assets/patrick/TilemapGather.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
+
+public class TilemapGather : MonoBehaviour
+{
+
+    public bool[,] nodes;
+
+    private void Awake()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += gatherTiles;
+    }
+
+
+    private void gatherTiles(Scene scene, LoadSceneMode mode)
+    {
+        Tilemap tilemap = GetComponent<Tilemap>();
+
+        BoundsInt bounds = tilemap.cellBounds;
+        TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+        Vector3 mapSize = tilemap.size;
+
+        TileBase[,] tiles2D = new TileBase[(int) mapSize[0],(int) mapSize[1]];
+
+        for (int i = 0; i < allTiles.Length; i++)
+        {
+            tiles2D[i / (int) mapSize[0], i % (int) mapSize[0]] = allTiles[i];
+            //if (allTiles[i].)
+        }
+        
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+}
