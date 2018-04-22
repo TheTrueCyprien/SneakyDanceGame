fbbc8e387067a04d3948ac4b171e54d3759a7a02
jazgardev <jazgar.dev@gmail.com>
2018-04-22 02:19:24 +0200

    Cone detection for enemies
    cones can now detect players and tell the parent object

---
+++ SneakyDanceGame/Assets/Scripts/OnConeCollision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public interface OnConeCollision : IEventSystemHandler
+{
+
+    // functions that can be called via the messaging system
+    void OnConeCollision(Collision2D collision);
+}
+
